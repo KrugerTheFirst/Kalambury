26eedd5d709d00f22cb53b9589fd37e68d496ede
Kruger <r.r.s.orzel@gmail.com>
2017-04-19 22:29:14 +0200

    (html) creating gulpfile #3

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,5 @@
 var gulp = require('gulp');
-
+var concat = require('gulp-concat');
 
 gulp.task('images', function() {
     var imgSrc = './src/img/*',
@@ -8,3 +8,25 @@ gulp.task('images', function() {
     return gulp.src(imgSrc)
         .pipe(gulp.dest(imgDst));
 });
+
+gulp.task('css', function(){
+    var cssSrc = './src/main.css',
+        cssDst = './build/main.css/';
+
+    return gulp.src(cssSrc)
+        .pipe(gulp.dest(cssDst));
+});
+
+gulp.task('minify-css', function() {
+    return gulp.src('./src/main.css')
+                .pipe(gulp.dest('./build/main.css'));
+});
+
+gulp.task('scripts', function() {
+    return gulp.src('.src/*.js')
+        .pipe(concat('all.js'))
+        .pipe(gulp.dest('./build/'));
+});
+
+
+gulp.task('build', ['images', 'css', 'minify-css', 'scripts']);
